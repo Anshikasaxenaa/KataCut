5d54a7404a838e151286dbc5fddcd948b5bc22d2
Anshikasaxenaa <anshikasaxena314@gmail.com>
2026-08-04 15:30:33 +0530

    Connection pool fix

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -8,5 +8,13 @@ dotenv.config();
 const connectionString =
   process.env.DATABASE_URL || "postgres://localhost:5432/katacut";
 
-const client = postgres(connectionString);
+const globalForDb = globalThis as unknown as {
+  _postgresClient: ReturnType<typeof postgres> | undefined;
+};
+
+const client = globalForDb._postgresClient || postgres(connectionString);
+if (process.env.NODE_ENV !== "production") {
+  globalForDb._postgresClient = client;
+}
+
 export const db = drizzle(client, { schema });
